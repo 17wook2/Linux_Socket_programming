b6155910a96bea326dc90e0477865e13358b754c
17wook2 <linuxs79267926@gmail.com>
2021-04-19 19:54:37 +0900

    add chating system

--- server.c
+++ server.c
@@ -1,48 +1,105 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <strings.h>
+#include <fcntl.h>
 #include <sys/socket.h>
+#include <sys/file.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
-#include <unistd.h> // for close
-#define BUF_SIZE 1024
+#include <unistd.h>
+#include <time.h>
+#include <pthread.h>
+
+#define MAXLINE 1000
+#define MAX_SOCK 1024 // 솔라리스의 경우 64
+
+int tcp_listen(int address, int port, int backlog);
 int main(int argc, char *argv[])
 {
-  int server_socket, client_socket;
-  struct sockaddr_in server_address, client_address;
-  socklen_t size;
-  int recv_length;
-  char buffer[BUF_SIZE];
-  server_socket = socket(AF_INET, SOCK_STREAM, 0);
-  if (server_socket == -1)
+  int listen_sock; // 서버의 리슨 소켓
+  struct sockaddr_in cliaddr;
+  char bufmsg[MAXLINE]; // 메시지부분
+  char bufall[MAXLINE];
+  // char buf[MAXLINE + 1]; //클라이언트에서 받은 메시지
+  int nbyte, accp_sock, addrlen = sizeof(struct sockaddr_in);
+  fd_set read_fds; //읽기를 감지할 fd_set 구조체
+  pthread_t a_thread;
+
+  if (argc != 2)
   {
-    printf("Could not create socket");
+    printf("사용법 :%s port\n", argv[0]);
+    exit(0);
   }
-  server_address.sin_family = AF_INET;
-  server_address.sin_addr.s_addr = 0;
-  server_address.sin_port = htons(atoi(argv[1]));
-  memset(&(server_address.sin_zero), 0, 8); // 메모리에 주소 0으로 초기화
 
-  bind(server_socket, (struct sockaddr *)&server_address, sizeof(struct sockaddr));
+  // init_listenSocket(address, port, backlog) 함수 호출
+  listen_sock = tcp_listen(INADDR_ANY, atoi(argv[1]), 5);
+  accp_sock = accept(listen_sock, (struct sockaddr *)&cliaddr, &addrlen);
+  //스레드 생성
+  // pthread_create(&a_thread, NULL, thread_function, (void *)NULL);
 
-  listen(server_socket, 3);
-  printf("server opened, waiting for client");
   while (1)
   {
-    size = sizeof(struct sockaddr_in);
-    client_socket = accept(server_socket, (struct sockaddr *)&client_address, &size);
+    FD_ZERO(&read_fds);
+    FD_SET(0, &read_fds);
+    FD_SET(accp_sock, &read_fds);
+    // if (accp_sock == -1)
+    //   errquit("accept fail");
 
-    send(client_socket, "Hello Client", 20, 0);
-    printf("Client Info : IP %s, Port %d\n", inet_ntoa(client_address.sin_addr), ntohs(client_address.sin_port));
-
-    recv_length = recv(client_socket, &buffer, BUF_SIZE, 0);
-    while (recv_length > 0)
+    if (select(accp_sock + 1, &read_fds, NULL, NULL, NULL) < 0)
+      printf("select Fail");
+    if (FD_ISSET(accp_sock, &read_fds))
     {
-      printf("From Client : %s\n", buffer);
-      recv_length = recv(client_socket, &buffer, BUF_SIZE, 0);
+      int nbyte;
+      if ((nbyte = recv(accp_sock, bufmsg, MAXLINE, 0)) > 0)
+      {
+        bufmsg[nbyte] = 0;
+        write(1, "\033[0G", 4);        //커서의 X좌표를 0으로 이동
+        printf("%s", bufmsg);          //메시지 출력
+        fprintf(stderr, "\033[1;32m"); //글자색을 녹색으로 변경
+      }
     }
-    close(client_socket);
+    if (FD_ISSET(0, &read_fds))
+    {
+      if (fgets(bufall, MAXLINE, stdin))
+      {
+        // fprintf(stderr, "\033[1;33m"); //글자색을 노란색으로 변경
+        // fprintf(stderr, "\033[1A");    //Y좌표를 현재 위치로부터 -1만큼 이동
+        // printf("%s", bufmsg); //메시지에 현재시간 추가
+        if (send(accp_sock, bufall, strlen(bufall), 0) < 0)
+          puts("Error : Write error on socket.");
+      }
+    }
   }
 
   return 0;
+}
+
+// listen 소켓 생성 및 listen
+int tcp_listen(int address, int port, int backlog)
+{
+  int listen_socket;
+  struct sockaddr_in servaddr;
+
+  listen_socket = socket(AF_INET, SOCK_STREAM, 0);
+  if (listen_socket == -1)
+  {
+    perror("socket creation fail");
+    exit(1);
+  }
+  // servaddr 구조체의 내용 세팅
+  memset(&servaddr, 0, sizeof(servaddr));
+  servaddr.sin_family = AF_INET;
+  servaddr.sin_addr.s_addr = htonl(address);
+  servaddr.sin_port = htons(port);
+
+  // binding
+  if (bind(listen_socket, (struct sockaddr *)&servaddr, sizeof(servaddr)) < 0)
+  {
+    perror("binding fail");
+    exit(1);
+  }
+  // 클라이언트로부터 연결요청을 기다림
+  listen(listen_socket, backlog);
+  return listen_socket;
 }
